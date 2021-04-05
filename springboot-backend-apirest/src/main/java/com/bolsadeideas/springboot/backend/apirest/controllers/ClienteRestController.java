cfc43644b7b99f401ebdf95221854561faddfe69
Henry Stivent <henrystiven7@hotmail.com>
2021-04-05 00:45:47 -0500

    Upload images and add the field date of birth

--- springboot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ springboot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,18 +1,30 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,7 +35,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.service.IClienteService;
@@ -35,6 +49,8 @@ public class ClienteRestController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log = LoggerFactory.getLogger(ClienteRestController.class);
 	
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
@@ -145,7 +161,19 @@ public class ClienteRestController {
 	@DeleteMapping("clientes/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 		Map<String, Object> response = new HashMap<>();
-		try {			
+		try {
+			Cliente cliente = clienteService.findById(id);
+			
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			if (nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+				
+				Path rutaFotoAnterior = Paths.get("/home/hstivent05/Pictures").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}
+			}
 			clienteService.delete(id);
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al eliminar el registro en la base de datos");
@@ -155,5 +183,73 @@ public class ClienteRestController {
 		
 		response.put("mensaje", "El cliente eliminado con exito!");
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping("clientes/upload")
+	public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		Cliente cliente = clienteService.findById(id);
+		
+		if (!archivo.isEmpty()) {
+			
+			String nombreArchivo = UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename().replace(" ", "");
+			Path rutaArchivo = Paths.get("/home/hstivent05/Pictures").resolve(nombreArchivo).toAbsolutePath();
+			log.info(rutaArchivo.toString());
+			
+			try {
+				Files.copy(archivo.getInputStream(), rutaArchivo);
+			} catch (IOException e) {
+				response.put("message", "Error en el servidor al subir el archivo: ".concat(nombreArchivo));
+				response.put("error", e.getMessage().concat(" : ").concat(e.getCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			if (nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+				
+				Path rutaFotoAnterior = Paths.get("/home/hstivent05/Pictures").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}
+			}
+			
+			
+			cliente.setFoto(nombreArchivo);
+		
+			clienteService.save(cliente);
+			
+			response.put("cliente", cliente);
+			response.put("message", "Has subido la imagen correctamente:".concat(nombreArchivo));
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("uploads/img/{nombreFoto:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto) {
+		
+		Path rutaArchivo = Paths.get("/home/hstivent05/Pictures").resolve(nombreFoto).toAbsolutePath();
+		log.info(rutaArchivo.toString());
+		Resource recurso = null;
+		
+		try {
+			recurso = new UrlResource(rutaArchivo.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		if (!recurso.exists() && recurso.isReadable()) {
+			throw new RuntimeException("Error no se pudo cargar la imagen: " + nombreFoto);
+		}
+		
+		HttpHeaders cabecera = new HttpHeaders();
+		cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+		
+		return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
 	}
 }
--- springboot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ springboot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -14,69 +14,101 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="clientes")
+@Table(name = "clientes")
 public class Cliente implements Serializable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@NotEmpty
-	@Size(min=4)
-	@Column(nullable=false)
+	@Size(min = 4)
+	@Column(nullable = false)
 	private String nombre;
 	@NotEmpty
-	@Size(min=4)
+	@Size(min = 4)
 	private String apellido;
 	@NotEmpty
 	@Email
-	@Column(nullable=false, unique=true)
+	@Column(nullable = false, unique = true)
 	private String email;
+
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	private Date datebirth;
+
+	private String foto;
 	
-	@Column(name="created_at")
+	@Column(name = "created_at")
 	@Temporal(TemporalType.DATE)
 	private Date createdAt;
-	
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = new Date();
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public Date getDatebirth() {
+		return datebirth;
+	}
+
+	public void setDatebirth(Date datebirth) {
+		this.datebirth = datebirth;
+	}
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
 	public Date getCreatedAt() {
 		return createdAt;
 	}
+
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+
 }
